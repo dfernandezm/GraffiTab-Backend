37a3986bc96ff97ee20b8c787cfefe66f6a0c91e
david <morenza@gmail.com>
2015-10-28 18:06:41 +0000

    Read example Json from file for tests

--- src/test/java/com/graffitab/server/test/api/UserApiTest.java
+++ src/test/java/com/graffitab/server/test/api/UserApiTest.java
@@ -1,19 +1,28 @@
 package com.graffitab.server.test.api;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import javax.annotation.Resource;
+import java.io.IOException;
+import java.io.InputStream;
 
+import javax.annotation.Resource;
+import javax.transaction.Transactional;
+
+import org.apache.commons.io.IOUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -22,12 +31,15 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.graffitab.server.api.user.UserApiController;
 import com.graffitab.server.persistence.model.User;
 import com.graffitab.server.service.UserService;
 
 @ContextConfiguration({"classpath:spring-context-test.xml"})
 @RunWith(SpringJUnit4ClassRunner.class)
+@Rollback(value = true)
 @WebAppConfiguration
 public class UserApiTest {
 	
@@ -44,41 +56,63 @@ public class UserApiTest {
 	    @Before
 	    public void setUp() {
 	        this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
-	        createUser();
 	    }
 	    
 	    @After
 	    public void clear() {
-	    	deleteUser();
+	    	// Nothing to do
 	    }
 	 
 	    @Test
+	    @Transactional
 	    public void getUserByIdTest() throws Exception {
+	    	createUser();
 	        mockMvc.perform(get("/api/users/{id}",testUser.getId()).accept(MediaType.APPLICATION_JSON))
 	                .andExpect(status().isOk())
 	                .andExpect(content().contentType("application/json;charset=UTF-8"))
 	                .andExpect(jsonPath("$.user.id").value(testUser.getId().intValue()));
+	        
 	    }
 	    
-	    private User createUser() {
+	    @Test
+	    @Transactional
+	    public void createUserTest() throws Exception {
+	    	fillTestUser();
+	    	InputStream in = this.getClass().getResourceAsStream("/api/user.json");
+	    	String json = IOUtils.toString(in);
+	    
+	    	mockMvc.perform(post("/api/users/register")
+	                .contentType("application/json;charset=UTF-8")
+	                .content(json))
+	                .andExpect(status().is(201))
+	                .andExpect(content().contentType("application/json;charset=UTF-8"))
+	                .andExpect(jsonPath("$.user.id").isNotEmpty())
+	                .andExpect(jsonPath("$.user.username").isNotEmpty())
+    				.andExpect(jsonPath("$.user.email").isNotEmpty());		
+	    }
+	    
+	    private User fillTestUser() {
 	    	testUser = new User();
 	    	testUser.setFirstName("a");
 	    	testUser.setLastName("b");
 	    	testUser.setEmail("a@a.com");
 	    	testUser.setUsername("ab");
 	    	testUser.setPassword("pass");
-	    	userService.persist(testUser);
 	    	return testUser;
 	    }
 	    
-	    private void deleteUser() {
-	    	if (testUser != null) {
-	    		userService.remove(testUser.getId());
-	    	}
+	    private User createUser() {
+	    	fillTestUser();
+	    	userService.persist(testUser);
+	    	return testUser;
 	    }
 	 
+	    
+	    
+	 
+	    
 	    @Configuration
-	    @EnableWebMvc
+	    @ImportResource("classpath:graffitab-servlet-test.xml")
 	    public static class TestConfiguration {
 	 
 	        @Bean
