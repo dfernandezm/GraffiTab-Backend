d1ec5b78720a4dff3b55d9b6cc9a48083da856cb
david <morenza@gmail.com>
2015-10-29 23:56:10 +0000

    Reorder cs03 migration to get Heroku Db to work (weird it works locally)

--- src/main/resources/database-update/script01.sql
+++ src/main/resources/database-update/script01.sql
@@ -7,11 +7,11 @@ ALTER TABLE person MODIFY password varchar(300);
 
 --changeset david:v100cs03
 RENAME TABLE person TO gt_user;
-ALTER TABLE gt_user ADD CONSTRAINT gt_user_avatar_fk FOREIGN KEY (avatarId) REFERENCES avatar(id);
-ALTER TABLE gt_user ADD CONSTRAINT gt_user_cover_fk FOREIGN KEY (coverId) REFERENCES cover(id);
 ALTER TABLE gt_user CHANGE externalId external_id varchar(1000) COLLATE utf8mb4_unicode_ci DEFAULT NULL;
 ALTER TABLE gt_user CHANGE avatarId avatar_id bigint(20) DEFAULT NULL;
 ALTER TABLE gt_user CHANGE coverId cover_id bigint(20) DEFAULT NULL;
+ALTER TABLE gt_user ADD CONSTRAINT gt_user_avatar_fk FOREIGN KEY (avatar_id) REFERENCES avatar(id);
+ALTER TABLE gt_user ADD CONSTRAINT gt_user_cover_fk FOREIGN KEY (avatar_id) REFERENCES cover(id);
 
 --changeset david:v100cs04
 ALTER TABLE avatar CHANGE image image varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL;
