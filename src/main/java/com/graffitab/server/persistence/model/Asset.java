4bf1d94adaea9840bd627944e2f329d05c51f834
david <morenza@gmail.com>
2016-03-02 20:35:15 +0000

    Change to Hibernate Annotations

--- src/main/java/com/graffitab/server/persistence/model/Asset.java
+++ src/main/java/com/graffitab/server/persistence/model/Asset.java
@@ -1,5 +1,14 @@
 package com.graffitab.server.persistence.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import com.graffitab.server.persistence.dao.Identifiable;
 import com.graffitab.server.util.GuidGenerator;
 
@@ -7,7 +16,11 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter @EqualsAndHashCode
+@Getter
+@Setter
+@EqualsAndHashCode
+@Entity
+@Table(name = "asset")
 public class Asset implements Identifiable<Long> {
 
 	private static final long serialVersionUID = 1L;
@@ -16,10 +29,16 @@ public class Asset implements Identifiable<Long> {
 		AVATAR, COVER, GRAFFITI;
 	}
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
+
+	@Column(name = "guid", nullable = false)
 	private String guid;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "asset_type", nullable = false)
 	private AssetType assetType;
-	private User user;
 
 	@Override
 	public Long getId() {
