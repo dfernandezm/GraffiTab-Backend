2680d788222f6b4dee622e2c6f260224dbab7f4a
david <morenza@gmail.com>
2016-03-31 00:17:29 +0100

    Added back the status endpoint. Task to wake up Heroku from CircleCI.

--- src/main/java/com/graffitab/server/api/controller/BaseApiController.java
+++ src/main/java/com/graffitab/server/api/controller/BaseApiController.java
@@ -1,12 +1,12 @@
 package com.graffitab.server.api.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.graffitab.server.api.dto.StatusDto;
 
-@Controller
+@RestController
 public class BaseApiController {
 
 	@RequestMapping(value = "/status", method = RequestMethod.GET)
