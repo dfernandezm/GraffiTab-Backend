79b8c9553169c65766544f27fbe33e075d8d5a76
lucia <lucia_c_l@hotmail.com>
2016-04-04 22:01:37 +0100

    Closes #35 Username validation

--- src/main/java/com/graffitab/server/api/errors/ResultCode.java
+++ src/main/java/com/graffitab/server/api/errors/ResultCode.java
@@ -3,16 +3,21 @@ package com.graffitab.server.api.errors;
 public enum ResultCode {
 
 	OK(200),
-	BAD_REQUEST(400),
 	UNSUPPORTED_FILE_TYPE(400),
-	USER_NOT_LOGGED_IN(401),
-	USER_NOT_OWNER(401),
 	INCORRECT_PASSWORD(403),
 	NOT_FOUND(404),
 	USER_NOT_FOUND(404),
 	STREAMABLE_NOT_FOUND(404),
 	COMMENT_NOT_FOUND(404),
 	TOKEN_EXPIRED(406),
+	BAD_REQUEST(400),
+	EMPTY_MANDATORY_FIELD(400),
+	INVALID_USERNAME(400),
+	INVALID_EMAIL(400),
+	USERNAME_ALREADY_IN_USE(400),
+	EMAIL_ALREADY_IN_USE(400),
+	USER_NOT_LOGGED_IN(401),
+	USER_NOT_OWNER(401),
 	USER_NOT_IN_EXPECTED_STATE(406),
 	ALREADY_EXISTS(409),
 	MAXIMUM_LOGIN_ATTEMPTS(403),
@@ -28,7 +33,7 @@ public enum ResultCode {
 		return this.name();
 	}
 
-	private ResultCode(Integer statusCode) {
+	ResultCode(Integer statusCode) {
 		this.statusCode = statusCode;
 	}
 }
--- src/main/java/com/graffitab/server/service/user/UserValidationService.java
+++ src/main/java/com/graffitab/server/service/user/UserValidationService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
+import com.graffitab.server.api.errors.RestApiException;
+import com.graffitab.server.api.errors.ResultCode;
 import com.graffitab.server.persistence.model.user.User;
 
 @Service
@@ -18,22 +20,58 @@ public class UserValidationService {
 	public Boolean validateUser(User user) {
 
 		boolean validationResult = false;
-
+		String errorText = "";
+		ResultCode resultCode = ResultCode.BAD_REQUEST;
+		
 		if ( StringUtils.isEmpty(user.getUsername()) || StringUtils.isEmpty(user.getEmail()) ||
 			 StringUtils.isEmpty(user.getFirstName()) || StringUtils.isEmpty(user.getLastName()) ||
 			 StringUtils.isEmpty(user.getPassword())) {
 
 			validationResult = false;
-
+			errorText = "Empty mandatory field." ;
+			resultCode = ResultCode.EMPTY_MANDATORY_FIELD;
+			
+		} else if (!user.getUsername().matches("[A-Za-z0-9-_]+") ||
+				    user.getUsername().length() < 3 ||
+				    user.getUsername().length() > 25){
+			
+			validationResult = false;
+			errorText = "Invalid username";
+			resultCode = ResultCode.INVALID_USERNAME;
+			
+		} else if(!user.getEmail().matches("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")){
+			
+			validationResult = false;
+			errorText = "Invalid email";
+			resultCode = ResultCode.INVALID_EMAIL;
+			
 		} else {
 
-			if (isUsernameTaken(user.getUsername(), user.getId()) || isEmailTaken(user.getEmail(), user.getId()) ){
+			if (isUsernameTaken(user.getUsername(), user.getId())  ){
+				
 				validationResult = false;
-			} else {
+				errorText="Username already in use.";
+				resultCode = ResultCode.USERNAME_ALREADY_IN_USE;
+				
+			} else if(isEmailTaken(user.getEmail(), user.getId())){
+				
+				validationResult = false;
+				errorText="Email already in use.";
+				resultCode = ResultCode.EMAIL_ALREADY_IN_USE;
+				
+			} else { 
+				
 				validationResult = true;
+				
 			}
 		}
-
+		
+		if (!validationResult) {
+			
+			throw new RestApiException(resultCode, errorText);
+			
+		}
+		
 		return validationResult;
 	}
 
