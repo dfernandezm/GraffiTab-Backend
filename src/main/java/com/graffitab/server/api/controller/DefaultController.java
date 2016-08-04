09ed0f0119602b918421cdfc0a6f89bde4333a24
david <morenza@gmail.com>
2016-08-04 20:59:49 +0100

    Fix compilation error not reported by IDEA.

--- src/main/java/com/graffitab/server/api/controller/DefaultController.java
+++ src/main/java/com/graffitab/server/api/controller/DefaultController.java
@@ -1,25 +1,17 @@
 package com.graffitab.server.api.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.graffitab.server.api.controller.pages.ResetPasswordForm;
-import com.graffitab.server.api.dto.ActionCompletedResult;
+import com.graffitab.server.api.dto.StatusDto;
 import com.graffitab.server.service.user.UserService;
-import com.sun.org.glassfish.external.statistics.annotations.Reset;
 import lombok.extern.log4j.Log4j2;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.interceptor.CacheOperationInvoker;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-import com.graffitab.server.api.dto.StatusDto;
+import javax.servlet.http.HttpServletRequest;
 
 @Log4j2
 @Controller
