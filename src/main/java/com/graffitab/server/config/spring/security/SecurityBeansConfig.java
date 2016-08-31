3dbf570794b08ecb1e76f93c2a9698e4a930aea2
david <morenza@gmail.com>
2016-08-31 21:20:30 +0100

    #95 Added missing import

--- src/main/java/com/graffitab/server/config/spring/security/SecurityBeansConfig.java
+++ src/main/java/com/graffitab/server/config/spring/security/SecurityBeansConfig.java
@@ -1,5 +1,6 @@
 package com.graffitab.server.config.spring.security;
 
+import com.graffitab.server.api.authentication.ProtocolCheckingFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
