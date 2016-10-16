77ab64c069f6766fedc8c5e6310a0a35a921ba19
david <morenza@gmail.com>
2016-10-16 20:34:37 +0100

    #101 Attempt to generate environment.sh from CircleCI env variables

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+rm environment.sh
 source deploy/deployEnvironment.sh
 
 # Default to Dev
@@ -27,6 +27,35 @@ then
  AMAZON_S3_BUCKET_NAME=$AMAZON_S3_DEV_BUCKET_NAME
 fi
 
+# Generate environment
+echo "== Generating environment =="
+
+echo "export AWS_ACCESS_KEY=$AWS_ACCESS_KEY" >> environment.sh
+echo "export AWS_SECRET_KEY=$AWS_SECRET_KEY" >> environment.sh
+echo "export FACEBOOK_APP_ID=$FACEBOOK_APP_ID" >> environment.sh
+echo "export FACEBOOK_APP_SECRET=$FACEBOOK_APP_SECRET" >> environment.sh
+echo "export PN_APNS_DEV_PASSWORD=$PN_APNS_DEV_PASSWORD" >> environment.sh
+echo "export PN_APNS_PROD_PASSWORD=$PN_APNS_PROD_PASSWORD" >> environment.sh
+echo "export PN_GCM_SENDER_KEY=$PN_GCM_SENDER_KEY" >> environment.sh
+echo "export SENDGRID_API_KEY=$SENDGRID_API_KEY" >> environment.sh
+echo "export DO_MYSQL_DB_HOST=$DO_MYSQL_DB_HOST" >> environment.sh
+echo "export DO_MYSQL_DB_PORT=$DO_MYSQL_DB_PORT" >> environment.sh
+echo "export DO_MYSQL_DB_USERNAME=$DO_MYSQL_DB_USERNAME" >> environment.sh
+echo "export DO_MYSQL_DB_PASSWORD=$DO_MYSQL_DB_PASSWORD" >> environment.sh
+echo "export DO_MYSQL_DB_NAME=$DO_MYSQL_DB_NAME" >> environment.sh
+echo "export DO_SERVER_PORT=$DO_SERVER_PORT" >> environment.sh
+echo "export DO_REDIS_HOST=$DO_REDIS_HOST" >> environment.sh
+echo "export DO_REDIS_PORT=$DO_REDIS_PORT" >> environment.sh
+echo "export DO_REDIS_PASSWORD=$DO_REDIS_PASSWORD" >> environment.sh
+echo "export DO_DEPLOYMENT_DIR=$DO_DEPLOYMENT_DIR" >> environment.sh
+echo "export DEPLOY_USERNAME=$DEPLOY_USERNAME" >> environment.sh
+echo "export SHARED_DIRECTORY=$SHARED_DIRECTORY" >> environment.sh
+echo "export AMAZON_S3_BUCKET_NAME=$AMAZON_S3_BUCKET_NAME" >> environment.sh
+echo "export DO_USER=$DO_USER" >> environment.sh
+
+echo "Generated environment file:"
+cat environment.sh
+
 echo "Generating JAR archive to deploy, env is $ENVNAME"
 ./gradlew stage -Penv=$ENVNAME
 
@@ -42,6 +71,7 @@ scp graffitab.jar $DO_USER@$DO_SRV01_DOMAIN:$DO_DEPLOYMENT_DIR
 scp digitalOcean/start.sh $DO_USER@$DO_SRV01_DOMAIN:$DO_DEPLOYMENT_DIR
 scp digitalOcean/stop.sh $DO_USER@$DO_SRV01_DOMAIN:$DO_DEPLOYMENT_DIR
 scp deploy/pollForStartup.sh $DO_USER@$DO_SRV01_DOMAIN:$DO_DEPLOYMENT_DIR
+scp environment.sh $DO_USER@$DO_SRV01_DOMAIN:~/
 
 echo "==== Stopping application - ${ENVIRON}01 ===="
 ssh $DO_USER@$DO_SRV01_DOMAIN 'source ~/environment.sh && chmod +x $DO_DEPLOYMENT_DIR/stop.sh' 2>&1
@@ -62,6 +92,7 @@ scp graffitab.jar $DO_USER@$DO_SRV02_DOMAIN:$DO_DEPLOYMENT_DIR
 scp digitalOcean/start.sh $DO_USER@$DO_SRV02_DOMAIN:$DO_DEPLOYMENT_DIR
 scp digitalOcean/stop.sh $DO_USER@$DO_SRV02_DOMAIN:$DO_DEPLOYMENT_DIR
 scp deploy/pollForStartup.sh $DO_USER@$DO_SRV02_DOMAIN:$DO_DEPLOYMENT_DIR
+scp environment.sh $DO_USER@$DO_SRV02_DOMAIN:~/
 
 echo "==== Stopping application - ${ENVIRON}02 ===="
 ssh $DO_USER@$DO_SRV02_DOMAIN 'source ~/environment.sh && bash $DO_DEPLOYMENT_DIR/stop.sh' 2>&1
